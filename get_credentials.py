6cccb16a99d4a9c35b34c424e00004a5ad0e5904
jiravani <j.iravani@gmail.com>
2017-09-28 16:00:52 -0600

    Create get_credentials.py

---
+++ get_credentials.py
@@ -0,0 +1,27 @@
+import sys
+import os
+import requests
+
+region = 'us-east-1'
+credentials_file = "/var/lib/jenkins/.aws/credentials"
+dir = '/var/lib/jenkins/.aws'
+
+credentials = requests.get('http://169.254.169.254/latest/meta-data/iam/security-credentials/jenkins-role')
+obj = credentials.json()
+
+access_key_id     = obj["AccessKeyId"]
+token             = obj["Token"]
+secret_access_key = obj["SecretAccessKey"]
+expiration        = obj["Expiration"]
+
+if not os.path.exists(dir):
+	os.makedirs(dir)
+
+with open(credentials_file, 'w') as f:
+	f.write('[jenkins_role]\n')
+	f.write('region = %s\n' % region)
+	f.write('output = json\n')
+	f.write('aws_session_token = %s\n' % token)
+	f.write('aws_access_key_id = %s\n' % access_key_id)
+	f.write('aws_secret_access_key = %s\n' % secret_access_key)
+	f.write('aws_session_expires_utc = %s\n' % expiration)
